868caa1bd7b9d4eba6b9731b68c28030ea49e340
Tim Parsons <tim@Tims-MacBook-Pro.local>
2019-09-23 23:04:52 -0700

    test pass

---
+++ toLowerCase/index.js
@@ -0,0 +1,8 @@
+
+//Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+var toLowerCase = function(str) {
+  return str.toLowerCase() 
+};
+
+module.exports = toLowerCase;
---
+++ toLowerCase/test.js
@@ -0,0 +1,13 @@
+const toLowerCase = require('./index.js')
+
+test('toLowerCase should equal "hello', () => {
+  expect(toLowerCase("Hello")).toEqual("hello");
+});
+
+test('toLowerCase should equal "hear', () => {
+  expect(toLowerCase("HEar")).toEqual("hear");
+});
+
+test('toLowerCase should equal "lovely', () => {
+  expect(toLowerCase("LOVELY")).toEqual("lovely");
+});
