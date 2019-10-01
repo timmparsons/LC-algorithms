75585be0f5f93cfbb9f1b7038336c9a95f1e8b4e
Tim Parsons <tim@Tims-MacBook-Pro.local>
2019-10-01 06:36:21 -0700

    new answer

--- SortArrayByParity/index.js
+++ SortArrayByParity/index.js
@@ -5,16 +5,17 @@ consisting of all the even elements of A, followed by all the
 odd elements of A. You may return any answer array that satisfies this condition. */
 
 function sortArrayByParity(A) {
-  let evenArray = [];
-  let oddArray = [];
-  for(let num of A) {
-    if(num % 2 == 0) {
-      evenArray.push(num)
-    } else {
-      oddArray.push(num)
-    }
-  }
-  return [...evenArray,...oddArray]
+  return [...A.filter(num => num % 2 == 0),...A.filter(num => num % 2 !== 0)]
+  // let evenArray = [];
+  // let oddArray = [];
+  // for(let num of A) {
+  //   if(num % 2 == 0) {
+  //     evenArray.push(num)
+  //   } else {
+  //     oddArray.push(num)
+  //   }
+  // }
+  // return [...evenArray,...oddArray]
 }
 
 module.exports = sortArrayByParity;
