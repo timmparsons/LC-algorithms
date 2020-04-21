1776da17a013948b6083e9cfb69e8cd9cb1aa9a2
Tim Parsons <timmparsons85@gmail.com>
2020-04-21 07:00:09 -0700

    added new linked list algo

---
+++ MiddleOfTheLinkedList/index.js
@@ -0,0 +1,27 @@
+//Given a non-empty, singly linked list with head node head, return a middle node of 
+//linked list. If there are two middle nodes, return the second middle node.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var middleNode = function(head) {
+  let slow = this.head;
+  let fast = this.head;
+
+  if(fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
+
+module.exports = middleNode;
